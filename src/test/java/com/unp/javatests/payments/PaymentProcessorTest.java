9d22da908b3fa386c6b1afc199bff5f99475d08b
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-07-10 10:20:23 -0500

    Organizacion con el setup

--- src/test/java/com/unp/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/unp/javatests/payments/PaymentProcessorTest.java
@@ -1,11 +1,21 @@
 package com.unp.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
 
     @Test
     public void payment_is_correct(){
@@ -16,7 +26,8 @@ public class PaymentProcessorTest {
         PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         boolean result = paymentProcessor.makePayment(1000);
-        assertTrue(paymentProcessor.makePayment(1000));
+
+        assertTrue(result);
     }
 
     @Test
@@ -26,7 +37,7 @@ public class PaymentProcessorTest {
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
         PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-        
+
         assertFalse(paymentProcessor.makePayment(1000));
     }
 
